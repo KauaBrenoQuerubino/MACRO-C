828b467d79f1307a615be64d3676d1ce852e0987
Kaua Breno <vaibetan009@gmail.com>
2026-05-26 15:50:45 -0300

    front adicionar chamado

--- FRONTEND/MC/src/app/components/chamados/chamados.component.ts
+++ FRONTEND/MC/src/app/components/chamados/chamados.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ListaChamadosComponent } from './lista-chamados/lista-chamados.component';
+import { AdicionarChamadosComponent } from "./adicionar-chamados/adicionar-chamados.component";
+import { DescricaoChamadoComponent } from "./descricao-chamado/descricao-chamado.component";
 
 @Component({
   selector: 'app-chamados',
-  imports: [ListaChamadosComponent],
+  imports: [ListaChamadosComponent, AdicionarChamadosComponent, DescricaoChamadoComponent],
   templateUrl: './chamados.component.html',
   styleUrl: './chamados.component.scss'
 })
